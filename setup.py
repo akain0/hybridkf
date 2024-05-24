91ff84d3e9d7ec0be643d3a01f67bd5ef915546f
Alec <alecmkain.com>
2024-05-24 14:19:49 -0400

    adding stuff

--- setup.py
+++ setup.py
@@ -13,4 +13,13 @@ setup(
         'scipy',
         'matplotlib',
     ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/akain0/hybridkf',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
